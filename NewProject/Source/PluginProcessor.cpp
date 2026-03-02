fcdc31498a3e2ac198c340c5adc0935aa80cc740
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2026-03-02 15:00:08 -0500

    changed default Env Decay value to 5000ms

--- NewProject/Source/PluginProcessor.cpp
+++ NewProject/Source/PluginProcessor.cpp
@@ -502,7 +502,7 @@ juce::AudioProcessorValueTreeState::ParameterLayout NewProjectAudioProcessor::cr
         juce::ParameterID(ParameterIDs::envDecay, 1),
         "Env Decay",
         juce::NormalisableRange<float>(0.0f, 5000.0f, 1.0f, 0.4f),
-        820.0f,
+        5000.0f,
         "ms",
         juce::AudioProcessorParameter::genericParameter,
         [](float value, int) { return juce::String(value, 1) + " ms"; }
